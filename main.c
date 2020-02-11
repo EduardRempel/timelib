b7b87639f25283990aed4ebe2e6a6337d6d78a8a
ifa92.petejona <j.peterburs@outlook.de>
2020-02-11 08:53:59 +0100

    add input_date

--- main.c
+++ main.c
@@ -8,19 +8,6 @@
 
 /** \brief
  *
- * \param day int*
- * \param month int*
- * \param year int*
- * \return void
- *
- */
-void input_date(int* day, int* month, int* year)
-{
-
-}
-
-/** \brief
- *
  * \param year int
  * \return int
  *
@@ -32,7 +19,7 @@ int is_leapyear(int year)
         return -1;
     }
 
-    return year%4 == 0 && year%100 != 0 || year%400 == 0;
+    return (year%4 == 0 && year%100) != 0 || year%400 == 0;
 }
 
 /** \brief
@@ -51,7 +38,8 @@ int get_days_for_month(int month, int year)
 
     int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    if(is_leapyear(year)){
+    if(is_leapyear(year))
+	{
         days[1] = 29;
     } else {
         days[1] = 28;
@@ -71,6 +59,36 @@ int get_days_for_month(int month, int year)
 int exist_date(int day, int month, int year)
 {
     return !(day < 1 || day > get_days_for_month(month, year) || year < 1582 || year > 2400);
+}
+
+/** \brief
+ *
+ * \param day int*
+ * \param month int*
+ * \param year int*
+ * \return void
+ *
+ */
+void input_date(int* day, int* month, int* year)
+{
+    do
+	{
+        *day = 0; *month = 0; *year = 0;
+
+	    printf("Bitte Tag eingeben: ");
+		scanf("%i", day);
+		fflush(stdin);
+
+		printf("Bitte Monat eingeben: ");
+		scanf("%i", month);
+		fflush(stdin);
+
+		printf("Bitte Jahr eingeben: ");
+		scanf("%i", year);
+		fflush(stdin);
+
+    }
+    while(!exist_date(*day, *month, *year));
 }
 
 /** \brief
@@ -103,5 +121,12 @@ int main()
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2020));
     printf("Tag des Jahres: %i\n", day_of_the_year(1, 3, 1996));
+	int day, month, year;
+	while(1)
+	{
+		input_date(&day, &month, &year);
+		printf("Tag des Jahres: %i\n", day_of_the_year(day, month, year));
+	}
+
     return 0;
 }
